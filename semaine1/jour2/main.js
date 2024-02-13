a0450e1bd502cfe7af92dc68ce762b54be417d26
Brasero <brandonricci4@gmail.com>
2024-02-13 17:22:36 +0100

    correction exo jour 2 apres midi

--- semaine1/jour2/main.js
+++ semaine1/jour2/main.js
@@ -120,3 +120,118 @@ console.log(pricesTTC_V2);
 const pricesTTC_V3 = phones.map(phone => ({...phone, priceTTC: ttc(phone.priceHT)}))
 
 console.log(pricesTTC_V3);
+
+/*
+07 Exercice square numbers
+Soit le point A suivant, calculez la distance de ce point à l'ensemble de chaques points de la liste positions.
+Vous donnerez les résultats dans un nouveau tableau distances.
+ */
+
+const A = [8.3, 7.5];
+const positions = [[1,1], [2, 2], [3, 4.5], [0, 9]];
+const distances = [];
+
+/*
+1) Rappelons comment on effectue le calcul de la distance avec une précision de deux chiffres après la virgule :
+ */
+
+const X = [1,2];
+const B = [4,1.5];
+
+const d = Math.floor( Math.sqrt( (X[0] - B[0] )**2 + (X[1] - B[1] )**2 ) * 100) / 100 ;
+
+//2) Trouver le point le plus éloigné du point A
+
+const calcDist = (a, b) => Math.floor( Math.sqrt( (a[0] - b[0] )**2 + (a[1] - b[1] )**2 ) * 100) / 100 ;
+
+for (const [x, y] of positions) {
+    distances.push({
+        point: [x, y],
+        distance: calcDist(A, [x, y])
+    })
+}
+
+console.log(distances);
+let futherPoint = {p: null, d: 0}
+
+for (const {point, distance} of distances) {
+    if(distance > futherPoint.d) {
+        futherPoint.d = distance
+        futherPoint.p = point
+    }
+}
+
+console.log(futherPoint)
+
+/*
+08 Exercice string
+1) Inversez la chaîne de caractères sentence ci-après.
+
+2) Comptez le nombre de caractères de chaque mot.
+
+3) Faites un script qui prend en argument une phrase et qui retourne dans un tableau le nombre de caractères de chaque mot. Vous ne compterez pas les espaces comme un caractère.
+
+Indication : utilisez la méthode split pour transformer la chaîne de caractères en tableau.
+ */
+
+const sentence = "Bonjour tout le monde, vous aimez JS ?";
+
+// partie 1 : Version Algorithmique
+
+const newSentence = [...sentence];
+let str = '';
+while (newSentence.length > 0) {
+    str += newSentence.pop();
+}
+
+console.log(str);
+
+//partie 1 : Version native
+
+str = [...sentence].reverse().join('');
+console.log(str);
+
+//partie 2
+const words = sentence.split(' ').map(word => ({word, count: word.length}))
+console.log(words);
+
+//partie 3
+const countChar = (sentence) => {
+    return sentence.split(' ').map(word => ({word, count: word.length}))
+}
+
+const phrase = 'Test de fonction';
+console.log(countChar(phrase));
+
+/*
+09 Exercice count letters
+Comptez chacune des lettres dans "Mississipi". Affichez le résultat dans une structure de données lisible.
+
+Généralisez et créez maintenant une fonction qui prend en paramètre une chaîne de caractères et qui retourne le nombre d'occurences de chacune de ses lettres.
+ */
+
+const word = "Mississipi";
+let stats = {};
+
+const searchLetters = new Set(word);
+
+console.log(searchLetters)
+
+for (const letter of searchLetters) {
+    let count = 0;
+
+    for(const p of word) {
+        if(letter == p) count++
+    }
+
+    stats[letter] = count;
+}
+
+console.log(stats)
+
+stats = {};
+
+[...searchLetters].map(letter => {
+    const regex = new RegExp(letter, "g");
+    stats = {...stats, [letter]: word.match(regex).length}
+})
